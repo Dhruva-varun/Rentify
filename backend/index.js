809933ed3c52b6ca74bc96e3a400d244d84c36f1
Varun T <varunvarun60954@gmail.com>
2025-03-28 10:26:10 +0530

    Fix issues

--- backend/index.js
+++ backend/index.js
@@ -21,11 +21,11 @@ app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/listing", listingRoute);
 
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
